e09732a0a3c7c572545ef3fd146943cb4be23af9
Ayeshnoorain <121870021+Ayeshnoorain@users.noreply.github.com>
2024-01-16 13:41:25 +0100

    Update dv_india_data.py
    @validators renamed to @field_validators

--- Data_validation/dv_india_data.py
+++ Data_validation/dv_india_data.py
@@ -2,17 +2,20 @@ import csv
 from datetime import datetime, date, time
 from typing import Optional, List
 
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, field_validator
 from pydantic.fields import Field
-from pydantic import validator
 
 
 # Step 1: Define the Pydantic model
+def valueError(param):
+    pass
+
+
 class SurveyRecord(BaseModel):
     ID: int
     user_id: str
     survey_id: int = Field(alias='survey_i')
-    mid: Optional[str]
+    mid: Optional[int]
     mid2: Optional[str]
     sessionid: Optional[str]
     dayMonth: int
@@ -23,23 +26,31 @@ class SurveyRecord(BaseModel):
     year: int
     date: date
     time: time
+    q888811_2 : str
 
-    @validator('date', pre=True)
+    @field_validator("date")
     def parse_date(cls, value):
         if value:
             return datetime.strptime(value, '%d/%m/%Y').date()
         return value
 
-    @validator('time', pre=True)
+    @field_validator("time")
     def parse_time(cls, value):
         if value:
             return datetime.strptime(value, '%H:%M:%S').time()
         return value
+
+    @field_validator("q888811_2")
+    def parse_q888811_2(cls, value):
+        if "“in the know”"in value:
+            raise valueError("Column value cannot have double quotes")
+        return value
+
 # Step 2: Function to read and parse the CSV file
 file_path = r'C:\Users\ayesha.noorain\PycharmProjects\PydanticProject\Dataset.csv'
 def read_csv(file_path: str) -> List[SurveyRecord]:
     records = []
-    with open(file_path, mode='r', encoding='utf-8') as file:
+    with open(file_path, mode='r', encoding='cp1252') as file:
         reader = csv.DictReader(file)
         for row in reader:
             try:
@@ -55,4 +66,3 @@ records = read_csv(file_path)
 # Example usage: Print the parsed records
 for record in records:
     print(record)
-
