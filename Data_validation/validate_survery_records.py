6fcc98d1c12551a5b369d4ad139f6bc32e024fa6
Ayeshnoorain <121870021+Ayeshnoorain@users.noreply.github.com>
2024-02-01 13:12:40 +0100

    Update validate_survery_records.py

--- Data_validation/validate_survery_records.py
+++ Data_validation/validate_survery_records.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pyodbc
-from pydantic import BaseModel, ValidationError, StringConstraints,ConfigDict
+from pydantic import BaseModel, ValidationError, StringConstraints,ConfigDict, Field
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base, Mapped
 from typing_extensions import Annotated
@@ -14,7 +14,8 @@ class Asics(Base):
     user_id: Mapped[str] = Column(String, primary_key=True, nullable=False, unique=True)
     gender: Mapped[str] = Column(String(5),nullable=False)
     age: Mapped[int] = Column(Integer)
-    #q65475: Column(String)
+    q123_4: Mapped[int] = Column(Integer, nullable=False)
+
 
 
 class SurveyRecord(BaseModel):
@@ -23,6 +24,7 @@ class SurveyRecord(BaseModel):
     user_id: Annotated[str, StringConstraints(max_length=40)]
     gender: str
     age: int
+    q123_4: int = Field(default=4, validate_default=True)
 
 
 def main():
@@ -42,11 +44,11 @@ def main():
     # Fetch data from the database
     cursor.execute("SELECT * FROM Asics_SM_2020_Dec_Set1")
     rows = cursor.fetchall()
-  
+
 
     # Apply validations
     try:
-        records = [SurveyRecord(user_id=row.user_id, gender=row.gender, age=row.age) for row in rows]
+        records = [SurveyRecord(user_id=row.user_id, gender=row.gender, age=row.age, q123_4=row.q123_4) for row in rows]
     except ValidationError as e:
         print(f"Validation error: {e}")
 
